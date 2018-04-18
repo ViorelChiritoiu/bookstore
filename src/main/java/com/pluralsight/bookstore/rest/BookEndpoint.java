728cb31055d09b2e0be41077fc291796406296ef
Viorel Chiritoiu <viorel.chiritoiu@invinceaext.com>
2018-04-18 14:02:43 +0300

    Rest added

--- src/main/java/com/pluralsight/bookstore/rest/BookEndpoint.java
+++ src/main/java/com/pluralsight/bookstore/rest/BookEndpoint.java
@@ -4,10 +4,12 @@ import com.pluralsight.bookstore.model.Book;
 import com.pluralsight.bookstore.repository.BookRepository;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.validation.constraints.Min;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -18,19 +20,30 @@ public class BookEndpoint {
     @Inject
     private BookRepository bookRepository;
 
-    public Book getBook(Long id) {
-
-        return bookRepository.find(id);
+    @GET
+    @Path("/{id : \\d+}")
+    @Produces(APPLICATION_JSON)
+    public Response getBook(@PathParam("id") @Min(1) Long id) {
+        Book book = bookRepository.find(id);
+        if(book == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(book).build();
     }
 
-    public Book createBook(Book book) {
-
-        return bookRepository.create(book);
+    @POST
+    @Consumes(APPLICATION_JSON)
+    public Response createBook(Book book, @Context UriInfo uriInfo) {
+        book = bookRepository.create(book);
+        URI createdURI = uriInfo.getBaseUriBuilder().path(book.getId().toString()).build();
+        return Response.created(createdURI).build();
     }
 
-    public void deleteBook(Long id) {
-
+    @DELETE
+    @Path("{id : \\d+}")
+    public Response deleteBook(@PathParam("id") @Min(1) Long id) {
         bookRepository.delete(id);
+        return Response.noContent().build();
     }
 
     @GET
@@ -38,7 +51,7 @@ public class BookEndpoint {
     public Response getBooks() {
         List<Book> books = bookRepository.findAll();
         if (books.size() == 0) {
-            return Response.noContent().build();
+            return Response.status(Response.Status.NO_CONTENT).build();
         }
         return Response.ok(books).build();
     }
@@ -48,7 +61,7 @@ public class BookEndpoint {
     public Response countBooks() {
         Long nbOfBooks = bookRepository.countAll();
         if (nbOfBooks == 0) {
-            return Response.noContent().build();
+            return Response.status(Response.Status.NO_CONTENT).build();
         }
         return Response.ok(nbOfBooks).build();
     }
